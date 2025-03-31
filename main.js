aef58e41f8b4c643b5666c291af5755ce07525df
meyersa <88554476+meyersa@users.noreply.github.com>
2025-03-31 10:49:05 -0400

    Refactor

--- main.js
+++ main.js
@@ -9,33 +9,7 @@ const {
 } = require("discord.js");
 const { EmbedBuilder } = require("discord.js");
 
-/**
- * Validate Date Entries
- * 
- * - Should be valid when parsed with Date()
- * - Should not equal nothing
- * 
- * @param {String} dateString to be parsed 
- * @returns True if valid, false if not
- */
-export function isValidDate(dateString) {
-  const date = new Date(dateString);
-  return !isNaN(date) && String(dateString).trim() !== "";
-}
-
-/**
- * Function to Validate Title 
- * - Should be less than 50 char
- * - Should be more than 1 char
- * - Should not be "Example"
- * 
- * @param {String} title to verify 
- * @returns 
- */
-export function isValidTitle(title) {
-  const trimTitle = title.trim();
-  return trimTitle.length > 1 && trimTitle.length < 50 && trimTitle !== "Example";
-}
+import { isValidDate, isValidTitle } from "./lib/helpers";
 
 /*
  * Calculate dynamic upcoming dates and elapsed time
